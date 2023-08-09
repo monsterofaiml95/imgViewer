2ae1b57bf05681a91a0be283ab6c597c3bf3ed13
monsterofaiml95 <135440369+monsterofaiml95@users.noreply.github.com>
2023-08-09 22:47:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+var studentId=prompt("Enter your Student Id ");
+var imgsource="https://erp.psit.ac.in/assets/img/Simages/"+studentId+".jpg";
+document.querySelector("img").setAttribute("src",imgsource);
+document.querySelector(".c p").textContent="Student Id : "+studentId;
